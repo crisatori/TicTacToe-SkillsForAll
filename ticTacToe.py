2b6b39b40191fa12a4d205a4cc23282edfb6db20
Cristopher F <30444850+crisatori@users.noreply.github.com>
2023-09-06 13:47:31 -0400

    style: add some line-space

--- ticTacToe.py
+++ ticTacToe.py
@@ -2,12 +2,12 @@ board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
 
 
 def display_board(board):
-    count = 0
     for row in board:
         for cell in range(len(row)):
-            print("|", row[cell], end=" ", flush=True)
+            print(" | ", row[cell], end=" ", flush=True)
             if row[cell] == row[-1]:
-                print("|")
+                print(" | ")
+                print("")
 
 
 display_board(board)
