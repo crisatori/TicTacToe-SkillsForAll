18ced36dbc10fe0138e1048a836670bba545e3b8
Cristopher F <30444850+crisatori@users.noreply.github.com>
2023-09-06 17:56:31 -0400

    Its playable now

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,24 +1,46 @@
-board = [["", "", ""], ["", "", ""], ["", "", ""]]
+board = [["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"]]
 
 
 def display_board(board):
+    a, b, c = board[0]
+    d, e, f = board[1]
+    g, h, i = board[2]
+    print(
+        f"""
+        {a} \t{b}\t{c}\n
+        {d}\t{e}\t{f}\n
+        {g}\t{h}\t{i}
+        """
+    )
+    return None
+
+
+# for row in board:
+#     for cell in enumerate(row):
+#         ind = cell[0]
+#         ele = cell[1]
+#         print(" | ", ele, end=" ", flush=True)
+#         if ind == 2:
+#             print(" | ")
+def make_list_of_free_fields(board=board) -> int:
+    pass
+    available_space = 0
     for row in board:
-        for cell in enumerate(row):
-            ind = cell[0]
-            ele = cell[1]
-            print(" | ", ele, end=" ", flush=True)
-            if ind == 2:
-                print(" | ")
+        for cell in row:
+            if cell == "x" or cell == "o":
+                available_space += 1
+    return available_space
 
 
 def enter_move(token, board: list[str] = board):
-    pass
+    row, element = None, None
     row = int(input("Type the row number *0 - 2* :"))
     element = int(input("Type element number *0 - 2* :"))
     board[row][element] = token
-    # The function accepts the board's current status, asks the user about their move,
-    # checks the input, and updates the board according to the user's decision.
+    display_board(board)
+    space = make_list_of_free_fields(board)
+    print(f"Players have made {space}/8 movements.")
 
 
-enter_move(token="u")
-display_board(board)
+for i in range(1, 8 + 1):
+    enter_move("x")
